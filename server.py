6eeb462cf432f548f482e2c11688be08697bfd70
NRDD <iyi.owoeye@yahoo.com>
2023-07-12 16:48:18 +0100

    Initial Commit

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+# import requests
+import smtplib
+
+app = Flask(__name__)
+
+MY_MAIL = "owoeyepeace4@gmail.com"
+PASSWORD = "tlydqnzfseqhwjhb"
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_MAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=email,
+                to_addrs=MY_MAIL,
+                msg=f"Subject:{subject}\n\n"
+                    f"Name: {name}\n"
+                    f"Email: {email}\n"
+                    f"Message: {message}\n"
+            )
+            return render_template("index.html", msg_sent=True)
+
+    else:
+        return render_template("index.html", msq_sent=False)
+    # return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
